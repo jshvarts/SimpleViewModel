519d7f3645c05de7c222304240a612725554d9ee
James Shvarts <jshvarts@gmail.com>
2017-06-30 21:06:25 -0400

    integrate ViewModel

--- app/src/main/java/com/shvartsy/simpleviewmodel/simpleviewmodel/MainActivity.java
+++ app/src/main/java/com/shvartsy/simpleviewmodel/simpleviewmodel/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shvartsy.simpleviewmodel.simpleviewmodel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.LifecycleActivity;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,21 +10,30 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends LifecycleActivity {
 
     @BindView(R.id.click_count_text)
     protected TextView clickCountText;
+
+    private ClickCounterViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        viewModel = ViewModelProviders.of(this).get(ClickCounterViewModel.class);
+        displayClickCount(viewModel.getCount());
     }
 
     @OnClick(R.id.increment_button)
     public void incrementClickCount(View button) {
-        int currentClickCount = Integer.parseInt(clickCountText.getText().toString());
-        clickCountText.setText(String.valueOf(++currentClickCount));
+        viewModel.setCount(viewModel.getCount() + 1);
+        displayClickCount(viewModel.getCount());
+    }
+
+    private void displayClickCount(int clickCount) {
+        clickCountText.setText(String.valueOf(clickCount));
     }
 }
